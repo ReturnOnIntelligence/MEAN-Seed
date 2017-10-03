ef33134e33dfe64c7e83f60b76d0b142202148f7
Alexander Vasyaev <Alexander.Vasyaev@returnonintelligence.com>
2017-10-03 12:58:30 +0300

    Fixed e2e tests

--- src/e2e/specs/home.component.e2e-spec.ts
+++ src/e2e/specs/home.component.e2e-spec.ts
@@ -10,17 +10,12 @@ describe('Home', () => {
     expect<any>(element(by.css('sd-home form input')).isPresent()).toEqual(true);
   });
 
-  it('should have a list of computer scientists', () => {
-    expect<any>(element(by.css('sd-home ul')).getText())
-      .toEqual('Edsger Dijkstra\nDonald Knuth\nAlan Turing\nGrace Hopper');
-  });
-
   it('should add a name to the list using the form', () => {
     element(by.css('sd-home form input')).sendKeys('Tim Berners-Lee');
     element(by.css('sd-home form button')).click();
 
     expect<any>(element(by.css('sd-home ul')).getText())
-      .toEqual('Edsger Dijkstra\nDonald Knuth\nAlan Turing\nGrace Hopper\nTim Berners-Lee');
+      .toEqual('Tim Berners-Lee');
   });
 
 });
